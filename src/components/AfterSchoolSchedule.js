ebbc900a990985104172d30ff7f253f06df6382d
seoulfire0119 <119gpt1@gmail.com>
2025-05-30 01:27:13 +0900

    Update AfterSchoolSchedule.js

--- src/components/AfterSchoolSchedule.js
+++ src/components/AfterSchoolSchedule.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 import { Table, Modal, Button, Form } from "react-bootstrap";
 // Assuming db is correctly configured in this path
 // import { db } from "../firebase"; 
